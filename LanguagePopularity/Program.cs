8354ae9c9fe18d5e0197723fa2d43e53dfa31938
Jordan Leeper <j.m.leep@gmail.com>
2022-09-13 12:04:53 -0500

    moved reddit api code into new file

--- LanguagePopularity/Program.cs
+++ LanguagePopularity/Program.cs
@@ -1,55 +1,23 @@
-﻿using System.Text.Json;
+﻿
+using LanguagePopularity;
 
-namespace LanguagePopularity
+while (true)
 {
-    class Program
+    Console.Write("Enter subreddit: ");
+    string? inputSubreddit = Console.ReadLine();
+
+    if (inputSubreddit != null)
     {
-        private static readonly HttpClient client = new();
-        static async Task Main(string[] args)
+        var subreddit = await RedditAPI.RetrieveSubredditDataAsync(inputSubreddit);
+        if (subreddit != null)
         {
-            while (true)
+            var subscribers = subreddit.Data.Subscribers.ToString("#,###");
+            if (subscribers.Equals(""))
             {
-                Console.Write("Enter subreddit: ");
-                string? inputSubreddit = Console.ReadLine();
-
-                if (inputSubreddit != null)
-                {
-                    var subreddit = await RetrieveSubredditDataAsync(inputSubreddit);
-                    if (subreddit != null)
-                    {
-                        var subscribers = subreddit.Data.Subscribers.ToString("#,###");
-                        if (subscribers.Equals(""))
-                        {
-                            subscribers = "0";
-                        }
-
-                        Console.WriteLine($"{subscribers} Subscribers\n");
-                    }
-                }
-            }
-        }
-
-        private static async Task<Subreddit?> RetrieveSubredditDataAsync(string subreddit)
-        {
-            try
-            {
-                var subredditTask = client.GetStreamAsync($"https://www.reddit.com/r/{subreddit}/about.json");
-                var subredditResults = await JsonSerializer.DeserializeAsync<Subreddit>(await subredditTask);
-                return subredditResults;
-            }
-            catch (Exception e)
-            {
-                if (e.Message.Contains("404"))
-                {
-                    Console.WriteLine("Subreddit not found.\n");
-                }
-                else
-                {
-                    Console.WriteLine(e.Message);
-                }
+                subscribers = "0";
             }
 
-            return null;
+            Console.WriteLine($"{subscribers} Subscribers\n");
         }
     }
 }
---
+++ LanguagePopularity/RedditAPI.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace LanguagePopularity
+{
+    internal static class RedditAPI
+    {
+        private static readonly HttpClient client = new();
+
+        internal static async Task<Subreddit?> RetrieveSubredditDataAsync(string subreddit)
+        {
+            try
+            {
+                var subredditTask = client.GetStreamAsync($"https://www.reddit.com/r/{subreddit}/about.json");
+                var subredditResults = await JsonSerializer.DeserializeAsync<Subreddit>(await subredditTask);
+                return subredditResults;
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains("404"))
+                {
+                    Console.WriteLine("Subreddit not found.\n");
+                }
+                else
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            return null;
+        }
+    }
+}
